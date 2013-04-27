0af9beddc0c36a422a92632c87585c08d0cbb220
Dennis Ranke <dennis.ranke@gmail.com>
2013-04-27 11:46:28 +0200

    emscripten version compiles and runs as well

--- makefile
+++ makefile
@@ -7,8 +7,18 @@ build/%.o: sources/%.cpp
 	mkdir -p build
 	g++ -g -c $< -MD -MF build/$*.d -o build/$*.o
 
+minimalism.html: build-ems/main.o
+	/home/dranke/tmp/emscripten/emcc -O2 -s ASM_JS=1 $^ -o $@
+
+build-ems/%.o: sources/%.cpp
+	mkdir -p build-ems
+	/home/dranke/tmp/emscripten/emcc -DEMSCRIPTEN -c $< -MD -MF build-ems/$*.d -o build-ems/$*.o
+
 clean:
 	rm -rf build
+	rm -rf build-ems
+	rm -f minimalism
+	rm -f minimalism.html
 
 -include build/*.d
 
--- sources/main.cpp
+++ sources/main.cpp
@@ -1,33 +1,50 @@
 #include <SDL/SDL.h>
 #include <GL/gl.h>
+#ifdef EMSCRIPTEN
+#	include <emscripten.h>
+#endif
+
+static bool quit = false;
+
+void run();
 
 int main() {
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_Surface* pScreen = SDL_SetVideoMode(800, 600, 32, SDL_OPENGL);
 
-	bool quit = false;
+#ifdef EMSCRIPTEN
+	emscripten_set_main_loop(run, 60, true);
+#else
 	while(!quit) {
-		SDL_Event event;
-		while(SDL_PollEvent(&event)) {
-			switch(event.type) {
-			case SDL_QUIT:
-				quit = true;
-				break;
-			case SDL_KEYDOWN:
-				switch(event.key.keysym.sym) {
-				case SDLK_ESCAPE:
-					quit = true;
-					break;
-				}
-				break;
-			}
-		}
-
-		glClearColor(0.125f, 0.25f, 0.125f, 1);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		SDL_GL_SwapBuffers();
+		run();
 	}
+#endif
 
 	SDL_Quit();
+}
+
+void run() {
+	SDL_Event event;
+	while(SDL_PollEvent(&event)) {
+		switch(event.type) {
+		case SDL_QUIT:
+			quit = true;
+			break;
+		case SDL_KEYDOWN:
+			switch(event.key.keysym.sym) {
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
+		}
+	}
+
+	static float c = 0;
+	c += 0.01f;
+	if(c > 1) c = 0;
+	glClearColor(0.125f, c, 0.125f, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	SDL_GL_SwapBuffers();
 }
